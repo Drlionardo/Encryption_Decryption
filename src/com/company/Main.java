e1c9e6fd904c772d5317b057004fb0360e4acf55
Drlionardo <d.iagupets@gmail.com>
2020-03-01 20:55:21 +0300

    Added structure for using different algorithms,  Support for:  *default Input/Output  *encrypt and decrypt mode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,79 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-	// write your code here
+       Operator op = new Operator(args);
+       op.operate();
+    }
+}
+class Operator {
+    /**
+     * If there is no -mode, the program should work in enc mode.
+     * If there is no -key, the program should consider that key = 0.
+     * If there is no -data,  the program should assume that the data is an empty string.
+     * -alg unicode/shift shift is default
+     */
+    private String mode = "enc";
+    private String alg = "shift";
+    private int key = 0;
+    private String data="";
+
+    public Operator(String[] args) {
+        //Read args:
+        for(String s : args){
+
+        }
+    }
+    public String readData(){
+        return this.data=data;
+    }
+    private void writeData() {
+        System.out.println(data);
+    }
+
+    public void operate() {
+        readData();
+        operateData();
+        writeData();
+        }
+
+    private void operateData() throws NullPointerException {
+        Algorithm a = null;
+        if(alg.equals("shift")){
+            a=new ShiftAlg();
+    }
+        else if(alg.equals("unicode")){
+            a=new UnicodeAlg();
+        }
+        assert a != null;
+        if(mode.equals("encrypt")) data = a.Encrypt(data);
+        else if(mode.equals("decrypt")) data = a.Decrypt(data);
+}
+
+
+}
+interface Algorithm{
+    String Encrypt(String data);
+    String Decrypt(String data);
+}
+class ShiftAlg implements Algorithm{
+    @Override
+    public String Encrypt(String data) {
+        return null;
+    }
+
+    @Override
+    public String Decrypt(String data) {
+        return null;
+    }
+}
+class UnicodeAlg implements Algorithm{
+    @Override
+    public String Encrypt(String data) {
+        return null;
+    }
+
+    @Override
+    public String Decrypt(String data) {
+        return null;
     }
 }
